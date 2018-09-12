33fbb8ab766ade710f73b20b12d51b969ad5f2b1
NirvanaB <42762167+NirvanaB@users.noreply.github.com>
2018-09-12 15:43:05 +0800

    wz

---
+++ files/page2/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,H,I,H,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(U,_(),V,_()),W,_(),X,_(Y,[])),Z,_(),ba,_());}; 
+var b="url",c="page2.html",d="generationDate",e=new Date(1536738170117.27),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="f662e40229404239adeb29d19bfae1df",q="type",r="Axure:Page",s="name",t="page2",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="center",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="near",I="imageVerticalAlignment",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="a",V="b",W="interactionMap",X="diagram",Y="objects",Z="masters",ba="objectPaths";
+return _creator();
+})());
